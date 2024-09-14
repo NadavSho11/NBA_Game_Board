2a3201f16a8a01fdde26b299f927b2552d132666
NadavSho11 <shohat34@gmail.com>
2024-09-14 04:07:39 +0300

    Renamed folder from OldFolderName to NewFolderName

--- website/__init__.py
+++ website/__init__.py
@@ -34,7 +34,7 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists('website/instance/' + DB_NAME):
         with app.app_context():
             db.create_all()
             print('Created Database!')
